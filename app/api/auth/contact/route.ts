40534393bae3d4a84fda39d43eded03cee9b4d6d
Ishan Saini <ishansaini0105@gmail.com>
2025-07-07 11:45:04 +0530

    The contact form route is done

--- app/api/auth/contact/route.ts
+++ app/api/auth/contact/route.ts
@@ -1,18 +1,93 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/DB";
 import z from "zod";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authoptions";
 
 export const Contactschema = z.object({
-
-    
-})
+  topic: z.string().min(12).max(100),
+  content: z.string().min(2).max(20),
+});
 
 export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const parsedBody = Contactschema.safeParse(data);
 
+  if (!parsedBody.success) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: parsedBody.error.format(),
+      },
+      {
+        status: 403,
+      }
+    );
+  }
 
+  const { content, topic } = parsedBody.data;
 
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user.id) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: "Please login",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+
+  const userid = session.user.id;
 
   try {
+    const exist = await prisma.user.findFirst({
+      where: {
+        id: Number(userid),
+      },
+    });
+
+    if (!exist) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "User not registered",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const contact = await prisma.contact.create({
+      data: {
+        topic: topic,
+        content: content,
+      },
+    });
+
+    if (!contact) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "OOps try again!!",
+        },
+        {
+          status: 411,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Form is submitted",
+      },
+      { status: 200 }
+    );
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json(
