92f3928795686f43670d0fc45a325098fd2b2078
Ishan Saini <ishansaini0105@gmail.com>
2025-07-14 22:23:02 +0530

    Update route.ts

--- app/api/verifyorder/route.ts
+++ app/api/verifyorder/route.ts
@@ -117,7 +117,7 @@ export async function POST(req: NextRequest) {
       });
     }
 
-    return NextResponse.json({ success: true, transaction, amount });
+    return NextResponse.json({ success: true, transaction,validateAmount });
   } catch (error) {
     console.error("Transaction error:", error);
     return NextResponse.json(
