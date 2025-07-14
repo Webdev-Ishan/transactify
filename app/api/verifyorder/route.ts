534f69d6e538ed953b96b0f51ee485d27f04fa83
Ishan Saini <ishansaini0105@gmail.com>
2025-07-14 21:40:20 +0530

    Still same error

--- app/api/verifyorder/route.ts
+++ app/api/verifyorder/route.ts
@@ -80,7 +80,7 @@ export async function POST(req: NextRequest) {
     const [transaction] = await prisma.$transaction([
       prisma.transaction.create({
         data: {
-          amount,
+          amount:Number(amount),
           Status: "COMPLETED",
           senderId,
           receiverId,
