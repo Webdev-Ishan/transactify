187f093f013de3028cd7e737faff832a4ce970cc
Ishan Saini <ishansaini0105@gmail.com>
2025-07-15 09:07:29 +0530

    The amount is changed tot he string

--- app/api/verifyorder/route.ts
+++ app/api/verifyorder/route.ts
@@ -77,8 +77,8 @@ export async function POST(req: NextRequest) {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
     const safeamount = Math.round(validatedAmount * 100); // ← ✅ Convert rupees to paise
-    console.log(safeamount);
-    console.log(typeof safeamount);
+
+    console.log(typeof safeamount.toString());
     // Step 4: Perform atomic transaction
     const [transaction] = await prisma.$transaction([
       prisma.transaction.create({
