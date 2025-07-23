8600f520eda0bd41877c208c1ea07fcbffc711e1
Ishan Saini <ishansaini0105@gmail.com>
2025-07-23 08:52:00 +0530

    THe smooth scroll is enabled

--- components/ui/lenis.tsx
+++ components/ui/lenis.tsx
@@ -1,2 +1,32 @@
-"use client";
-export * as Lenis from "lenis/react"
+// components/LenisProvider.tsx
+'use client'
+
+import { useEffect, useRef } from 'react'
+import Lenis from '@studio-freight/lenis'
+
+export default function LenisProvider() {
+  const lenisRef = useRef<Lenis | null>(null)
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    })
+
+    lenisRef.current = lenis
+
+    const raf = (time: number) => {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+
+  return null // no visible UI
+}
